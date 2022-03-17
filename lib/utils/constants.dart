0495abcbfcbb171f8c5215fac772fe0255513b19
Karim <kariimAhmed8@gmail.com>
2022-03-17 20:10:29 +0200

    Adding Constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+
+class Constants {
+  static var taskDescription = "Hi,"
+      "\n\nthe tasks listed below should be done on the second view."
+      "\n\n1. Integrate the AnimatedContainer below to the next view "
+      "so it can be reordered there too and it should not stop changing "
+      "his size and should not overlap the other boxes. "
+      "The space between the boxes should stay at all time the same. "
+      "\n\n2. The overflowing text should be completely visible inside the "
+      "red box and the height of the box should be adjusted to his content."
+      "\n\n3. Thrown exceptions during the development must be solved."
+      "\n\n4. Refactoring the code can be done if wanted but the "
+      "ReorderableGridView cannot be replaced."
+      "\n\nInfo: You get to the second view via the button on the bottom.";
+}
